cdd657f90c8dc271aeb1fc9f4af75cd287edd6fe
Jiuru Shao <shaojiuru@ucla.edu>
2019-02-12 11:29:27 -0800

    js - feedback form updates as user types

---
+++ app/javascript/image_sharing/test/components/FeedbackForm.spec.js
@@ -0,0 +1,30 @@
+/* eslint-env mocha */
+import 'jsdom-global/register';
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import { expect } from 'chai';
+import Footer from '../../components/Footer';
+import Adapter from 'enzyme-adapter-react-16';
+import FeedbackForm from "../../components/FeedbackForm";
+configure({ adapter: new Adapter() });
+
+describe('<FeedbackForm />', () => {
+  it('feedback form has name&comments', () => {
+    const wrapper = shallow(<FeedbackForm />);
+    let name = wrapper.find('#name');
+    let comments = wrapper.find('#comments');
+
+    expect(name).to.have.lengthOf(1);
+    expect(comments).to.have.lengthOf(1);
+    expect(name.prop('value')).to.equal('');
+    expect(name.prop('value')).to.equal('');
+
+    name.simulate('change', { target: { value: 'John Doe' } });
+    comments.simulate('change', { target: { value: 'One man’s crappy software is another man’s full time job.'}});
+    name = wrapper.find('#name');
+    comments = wrapper.find('#comments');
+
+    expect(name.prop('value')).to.equal('John Doe');
+    expect(comments.prop('value')).to.equal('One man’s crappy software is another man’s full time job.');
+  });
+});
