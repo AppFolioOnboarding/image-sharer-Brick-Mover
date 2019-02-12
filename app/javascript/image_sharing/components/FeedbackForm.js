6d36151ef8600f58116a156a1a5b0f7f15c472db
Jiuru Shao <shaojiuru@ucla.edu>
2019-02-12 15:44:26 -0800

    js - move @action methods to store

--- app/javascript/image_sharing/components/FeedbackForm.js
+++ app/javascript/image_sharing/components/FeedbackForm.js
@@ -2,22 +2,9 @@ import React, { Component } from 'react';
 import { Col } from "reactstrap";
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import { observer } from 'mobx-react'
-import { action } from 'mobx'
 
 @observer
 class FeedbackForm extends Component {
-
-  @action
-  handleNameChange = (event) => {
-    this.props.feedbackStore.name = event.target.value;
-  };
-
-  @action
-  handleCommentsChange = (event) => {
-    this.props.feedbackStore.comments = event.target.value;
-  };
-
-
   render() {
     return (
       <Col lg={{ size: 4, offset: 4 }}>
@@ -25,12 +12,12 @@ class FeedbackForm extends Component {
           <FormGroup>
             <Label for="name">Your name:</Label>
             <Input type="text" name="name" id="name"
-                   value={this.props.feedbackStore.name} onChange={this.handleNameChange} />
+                   value={this.props.feedbackStore.name} onChange={this.props.feedbackStore.handleNameChange} />
           </FormGroup>
           <FormGroup>
             <Label for="comments">Comments</Label>
             <Input type="textarea" name="comments" id="comments"
-                   value={this.props.feedbackStore.comments} onChange={this.handleCommentsChange} />
+                   value={this.props.feedbackStore.comments} onChange={this.props.feedbackStore.handleCommentsChange} />
           </FormGroup>
           <Button>Submit</Button>
         </Form>
--- app/javascript/image_sharing/stores/FeedbackStore.js
+++ app/javascript/image_sharing/stores/FeedbackStore.js
@@ -1,8 +1,18 @@
-import { observable } from 'mobx'
+import {action, observable} from 'mobx'
 
 class FeedbackStore {
   @observable name = '';
   @observable comments = '';
+
+  @action
+  handleNameChange = (event) => {
+    this.name = event.target.value;
+  };
+
+  @action
+  handleCommentsChange = (event) => {
+    this.comments = event.target.value;
+  };
 }
 
 const feedbackStore = new FeedbackStore();
